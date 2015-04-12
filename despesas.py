4d8c4a1dc46c335664c30859171764eb1be6edaf
Regis da Silva <regis.santos.100@gmail.com>
2015-04-12 19:29:07 -0300

    Separate html into template

--- despesas.py
+++ despesas.py
@@ -1,24 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    html = """\
-<!DOCTYPE html>
-<html>
-<body>
-	<h1>Bem-vindo ao Despesas</h1>
-	<footer>
-		<p>Copyright 2015 - Régis da Silva</p>
-		<p>Feito com <a href="http://flask.pocoo.org/">Flask</a></p>
-	</footer>
-</body>
-</html>\
-"""
-
-    return html
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run()
